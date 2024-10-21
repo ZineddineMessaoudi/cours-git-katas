7f714cd38b2cd32b55bd3d6631e12e7d2e580e8c
unknown <vladimir.deliled@gmail.com>
2024-10-21 11:21:17 +0200

    test

--- katas-11/katas-11-test-.js
+++ katas-11/katas-11-test-.js
@@ -1,3 +1,5 @@
 const nombrePremier = require("./katas-11.js");
 
-test('test katas-11', () => {expect(nombrePremier(5).toBe(false))});
+test('test katas-11', () => {
+    expect(nombrePremier(5)).toBe(false);
+});
