d3f3a8ceb93f7f051c1385233eb0f25d12f009e0
enzy <enzyxmxr@gmail.com>
2024-10-21 11:22:02 +0200

    kata1Test

---
+++ Kata1/kata1.js
@@ -0,0 +1,10 @@
+// Kata1
+
+function PaireOuInpaire(nombre){
+    if (nombre % 2 == 0) {
+        return "paire"
+    }
+    else return "inpaire"
+}
+
+Module.exports = PaireOuInpaire;
---
+++ Kata1/kata1.test.js
@@ -0,0 +1,5 @@
+const PaireOuInpaire = require('./kata1');
+
+test('Dit si le nombre 2 est paire',() => {
+    expect(PaireOuInpaire(2)),toBe(paire);
+});
