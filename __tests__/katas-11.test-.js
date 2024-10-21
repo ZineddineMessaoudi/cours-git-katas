f0438be1201348d344199d76ce91a321ee24c038
unknown <vladimir.deliled@gmail.com>
2024-10-21 11:40:51 +0200

    test final

--- __tests__/katas-11.test-.js
+++ __tests__/katas-11.test-.js
@@ -1,4 +1,4 @@
-const nombrePremier = require('./katas-11');
+const nombrePremier = require('../katas-11.js');
 
 test('nombrePremier retourne true pour un nombre premier', () => {
     expect(nombrePremier(7)).toBe(true);
