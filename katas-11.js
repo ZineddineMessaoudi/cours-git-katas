0281d19f8d4ffd00da9ef6d89be0eb4e16fa2def
unknown <vladimir.deliled@gmail.com>
2024-10-21 09:41:35 +0200

    katas-11

---
+++ katas-11.js
@@ -0,0 +1,13 @@
+function nombrePremier(nombre){
+    if (nombre <= 1){
+        return false ;
+    }
+    else {
+        for (let i = 2; i <= Math.sqrt(nombre); i++) {
+            if (nombre % i === 0) {
+            return false;
+            }
+        }
+    }
+    return true ;
+}
