7ac93213299b35803df78cf15199d1a1dae016bf
zineddine <zineddine.messaoudi@outlook.fr>
2024-10-21 11:37:17 +0200

    ajout test

--- kata-9/kata-9.js
+++ kata-9/kata-9.js
@@ -3,3 +3,5 @@ function deleteDoubles(arr) {
 }
 
 console.log(deleteDoubles([1, 2, 4, 4, 3, 3, 1, 5, 3, 3]))
+
+module.exports = deleteDoubles
---
+++ kata-9/kata-9.test.js
@@ -0,0 +1,7 @@
+const deleteDoubles = require("./kata-9.js")
+
+test("deleteDoubles", () => {
+  expect(deleteDoubles([1, 2, 4, 4, 3, 3, 1, 5, 3, 3])).toEqual([
+    1, 2, 4, 3, 1, 5,
+  ])
+})
