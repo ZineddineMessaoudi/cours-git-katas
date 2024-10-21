feff99f2daf540d3c47e91f2bfa84cf11f6ada5d
unknown <vladimir.deliled@gmail.com>
2024-10-21 11:48:21 +0200

    test final

--- __tests__/katas-test-12.js
+++ __tests__/katas-test-12.js
@@ -1,5 +1,5 @@
 const nombrePremier = require('../katas-12.js');
 
 test('Est le 2ème nombre le plus grand du tableau', () => {
-    expect(deuxiemePlusGrand([3,5,7,8,9])).toBe(8);
+    expect(deuxiemePlusGrand([3, 5, 7, 8, 9])).toBe(8);
 });
