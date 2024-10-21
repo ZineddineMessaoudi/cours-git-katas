4a45d9766dff42073c58838b14a80e4d2acc98c0
unknown <vladimir.deliled@gmail.com>
2024-10-21 09:53:40 +0200

    katas-12.js

---
+++ katas-12.js
@@ -0,0 +1,22 @@
+function deuxiemePlusGrand(tableau) {
+    if (tableau.length < 2) {
+        return false; 
+    }
+
+    let premier = tableau[0]; 
+    let second = -1; 
+
+    for (let i = 1; i < tableau.length; i++) { 
+        let nombre = tableau[i];
+
+        if (nombre > premier) {
+            second = premier; 
+            premier = nombre; 
+        } else if (nombre > second && nombre < premier) {
+            second = nombre; 
+        }
+    }
+
+    return second === -1 ? false : second;
+}
+
